6b5d2a8ddf0afb63a405d0d7467c797bf8acd53b
susuwatarin <galileo_galilei_00@yahoo.co.jp>
2016-06-11 23:52:23 +0900

    laravel_lesson 4
    ・ControllerからViewへ値を渡す

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,10 +8,22 @@ use App\Http\Requests;
 
 class PagesController extends Controller
 {
-    public function about(){
-      return view('pages.about');
-    }
+    // public function about(){
+    //   $data = [];
+    //   $data["first_name"] = "Harry";
+    //   $data["last_name"] = "Potter";
+    //   return view('pages.about', $data);
+    //   // return view('pages.about');
+    // }
     
+    public function about(){
+      $first_name = "Harry";
+      $last_name = "Potter";
+      return view('pages.about', compact('first_name', 'last_name'));
+      // return view('pages.about');
+    }
+
+
     public function contact(){
       return view("pages.contact");
     }
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -33,12 +33,19 @@
             .title {
                 font-size: 96px;
             }
+
+            .name {
+                font-size: 43px;
+            }
+
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
                 <div class="title">About Page</div>
+                <!-- <div class="name">For <?= $first_name ?> <?= $last_name ?></div> -->
+                <div class="name">For {{ $first_name }} {{ $last_name }}</div>
             </div>
         </div>
     </body>
